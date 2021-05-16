9302ca38b329f726920c80e963e66fbf1e1cc7a4
jpike <jacob@jacobpike.com>
2021-05-16 08:33:20 -0400

    Adding some main.cpp files for tests

---
+++ Containers/testing/main.cpp
@@ -0,0 +1,3 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "ContainersTests/Array2DTests.h"
---
+++ Graphics/testing/main.cpp
@@ -0,0 +1,4 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "Graphics/CameraTests.cpp"
+#include "Graphics/Object3DTests.cpp"
