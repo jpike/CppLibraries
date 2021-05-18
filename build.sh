5c0c0623fc1c98dea6d0d9a35c69e9663ee84eea
jpike <jacob@jacobpike.com>
2021-05-17 21:03:37 -0400

    Update build.sh
    Printing out GCC/Clang compiler versions.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 mkdir build
 
+g++ --version
+clang++ --version
+
 g++ -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
 
 #clang++ -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
 
 ar rcs build/libCppLibraries.a build/CppLibraries.o
-
-
