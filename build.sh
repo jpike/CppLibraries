acbe035c2067491a4ba8dcd29d75031ece001c25
jpike <jacob@jacobpike.com>
2021-05-17 21:06:19 -0400

    Update build.sh
    Trying printing out newer compiler versions.

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ mkdir build
 g++ --version
 clang++ --version
 
-g++ -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
+g++-11 --version
+clang++-11 --version
 
-#clang++ -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
+g++-11 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
+
+#clang++-11 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
 
 ar rcs build/libCppLibraries.a build/CppLibraries.o
