85c3b13aaec801722a487579108cd2678fc833bf
jpike <jacob@jacobpike.com>
2021-06-11 20:16:39 -0400

    Trying gcc-10 since gcc-11 removed

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ clang++ --version
 g++-11 --version
 clang++-11 --version
 
-#g++-11 -std=c++23 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
+g++-10 -std=c++23 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
 
-clang++-11 -std=c++20 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
+#clang++-11 -std=c++20 -c -o build/CppLibraries.o -x c++ CppLibraries.project -I Containers/code -I ErrorHandling -I ErrorHandling/code -I Filesystem -I Filesystem/code -I Graphics -I Graphics/code -I Math -I Math/code -I String -I String/code -I Windowing -I Windowing/code
 
 ar rcs build/libCppLibraries.a build/CppLibraries.o
